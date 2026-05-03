f7d1841cc1fb575bae8fc0d1d935bba4495a233b
Jeshua&Angelli <104967531+JeshuaAngelli@users.noreply.github.com>
2026-05-02 20:15:22 -0600

    Update script.js

--- script.js
+++ script.js
@@ -61,7 +61,7 @@ fetch(URL_GOOGLE_SHEETS, {
 }
 
 function actualizarCuentaAtras() {
-  const fechaBoda = new Date("October 9, 2026 18:45:00").getTime();
+  const fechaBoda = new Date("May 31, 2026 13:00:00").getTime();
   const ahora = new Date().getTime();
   const diferencia = fechaBoda - ahora;
 
